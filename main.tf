0c87d628a391c83ae8520ad202c5eaf408afea82
Dyanand Sodgir <dyanandsodgir851@gmail.com>
2025-12-25 11:37:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-demo"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
